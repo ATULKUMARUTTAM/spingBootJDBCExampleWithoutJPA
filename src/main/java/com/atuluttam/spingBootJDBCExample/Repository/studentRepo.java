869d717b04e2c5182456d19684a77f55475ab68a
ATULKUMARUTTAM <atulkumaruttam@gmail.com>
2024-08-26 23:42:21 +0530

    Initial commit

--- src/main/java/com/atuluttam/spingBootJDBCExample/Repository/studentRepo.java
+++ src/main/java/com/atuluttam/spingBootJDBCExample/Repository/studentRepo.java
@@ -13,58 +13,6 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-@Repository
-public class studentRepo {
-    @Autowired
-    private Student s;
-    @Autowired
-    private JdbcTemplate JT;
-
-    String sql = "insert into Student(roll, Name, marks) values(?,?,?)";
-    public void save(Student s)
-    {
-       int a =  JT.update(sql,s.getRoll(),s.getName(), s.getMarks());
-       System.out.println(a+ " Student saved in Database");
-    }
-    public List<Student> findAll()
-    {
-        String sql = "select * from Student";
-
-        return JT.query(sql, new RowMapper<Student>() {
-            @Override
-            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Student s = new Student();
-
-                s.setRoll(rs.getInt("roll"));
-              //  s.setRoll(rs.getInt(1));
-                s.setName(rs.getString("Name"));
-                //s.setName(rs.getString(2));
-               s.setRoll(rs.getInt("marks"));
-
-                //s.setRoll(rs.getInt(3));
-                return s;
-            }
-        });
-       //return new ArrayList<Student>();
-    }
-
-    public void deleteStudent(int r) {
-        String sql = "delete from Student where roll = ?";
-        int a =  JT.update(sql, r);
-        System.out.println(a+" Student record deleted");
-    }
-
-    public void updateStudent(Student s)
-    {
-        String sql = "update Student SET name = ?, marks = ? where roll = ?";
-       int a =  JT.update(sql, s.getRoll(),s.getName(), s.getMarks());
-        System.out.println(a+" Student record updated");
-    }
-
-
-}
-*/
 @Repository
 public class studentRepo {
     @Autowired
